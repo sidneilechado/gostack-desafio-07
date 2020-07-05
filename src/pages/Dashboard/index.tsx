0e973894e8f366896edca155e27eeafb70e46a7f
sid <juniorlechado@gmail.com>
2020-07-05 15:56:56 -0300

    Solved
    Signed-off-by: sid <juniorlechado@gmail.com>

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,144 @@
+import React, { useState, useEffect } from 'react';
+
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+import api from '../../services/api';
+
+import Header from '../../components/Header';
+
+import formatValue from '../../utils/formatValue';
+
+import { Container, CardContainer, Card, TableContainer } from './styles';
+
+interface Transaction {
+  id: string;
+  title: string;
+  value: number;
+  formattedValue: string;
+  formattedDate: string;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: Date;
+}
+
+interface TransactionUnformated {
+  id: string;
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: string;
+}
+
+interface Balance {
+  income: string;
+  outcome: string;
+  total: string;
+}
+
+interface TransactionsResponseDTO {
+  balance: Balance;
+}
+
+const Dashboard: React.FC = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
+
+  useEffect(() => {
+    async function loadTransactions(): Promise<void> {
+      const { data } = await api.get(`transactions`);
+
+      const balanceData: Balance = data.balance;
+
+      const balanceFormated: Balance = {
+        income: formatValue(Number(balanceData.income)),
+        outcome: formatValue(Number(balanceData.outcome)),
+        total: formatValue(Number(balanceData.total)),
+      };
+
+      const formattedTransactions: Transaction[] = data.transactions.map(
+        (transaction: TransactionUnformated) => {
+          const formattedValue = formatValue(Number(transaction.value));
+
+          const created_at = new Date(transaction.created_at); // eslint-disable-line
+          const formattedDate = new Intl.DateTimeFormat('pt-BR').format(created_at);
+
+          return {
+            ...transaction,
+            created_at, // eslint-disable-line
+            formattedDate,
+            formattedValue,
+          } as Transaction;
+        },
+      );
+
+      setBalance(balanceFormated);
+      setTransactions(formattedTransactions);
+    }
+
+    loadTransactions();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <CardContainer>
+          <Card>
+            <header>
+              <p>Entradas</p>
+              <img src={income} alt="Income" />
+            </header>
+            <h1 data-testid="balance-income">{balance.income}</h1>
+          </Card>
+          <Card>
+            <header>
+              <p>Saídas</p>
+              <img src={outcome} alt="Outcome" />
+            </header>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
+          </Card>
+          <Card total>
+            <header>
+              <p>Total</p>
+              <img src={total} alt="Total" />
+            </header>
+            <h1 data-testid="balance-total">{balance.total}</h1>
+          </Card>
+        </CardContainer>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome'
+                      ? `- ${transaction.formattedValue}`
+                      : transaction.formattedValue}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.formattedDate}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
